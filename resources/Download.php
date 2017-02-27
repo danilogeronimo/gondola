72858610f338c952849b6d2b7f4df9f3f8b79d1d
Danilo Geronimo <danilo.geronimo@hotmail.com>
2017-02-27 15:05:48 +0000

    download function

--- resources/Download.php
+++ resources/Download.php
@@ -1,4 +1,19 @@
 <?php 
-
-//need a script to zip the files
+if(isset($_POST['download'])){
+	
+	if(!empty($_POST['check_list'])) {
+		// Counting number of checked checkboxes.
+		$checked_count = count($_POST['check_list']);
+		echo "You have selected following ".$checked_count." option(s): <br/>";
+		// Loop to store and display values of individual checked checkbox.
+		foreach($_POST['check_list'] as $selected) {			
+			$content = file_get_contents('http:'.$selected);
+			$fp = fopen("./Downloads/t.webm","w");
+			fwrite($fp,$content);
+			fclose($fp);
+		}
+	}else{
+		echo "you selected nothing. :p";
+	}
+}
 ?>
